6b18851cdc60ea10a41be6b918dc365f1e72262f
OHHYUN <omplcie@gmail.com>
2021-10-05 15:51:52 +0900

    refactor(Ball): 야구공의 결과를 담는 객체 클래스

---
+++ src/main/java/baseball/Ball.java
@@ -0,0 +1,38 @@
+package baseball;
+
+public class Ball {
+
+	private int num;
+	private int strike;
+	private int ball;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public int getStrike() {
+		return strike;
+	}
+
+	public void setStrike(int strike) {
+		this.strike = strike;
+	}
+
+	public int getBall() {
+		return ball;
+	}
+
+	public void setBall(int ball) {
+		this.ball = ball;
+	}
+
+	public Ball(int num, int strike) {
+		this.num = num;
+		this.strike = strike;
+	}
+
+}
