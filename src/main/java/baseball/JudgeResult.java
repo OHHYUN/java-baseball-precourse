b9d53891493c593c782c260178ba34419123ae41
OHHYUN <omplcie@gmail.com>
2021-10-05 15:52:31 +0900

    refactor(Judge): 판단 결과를 담는 객체 클래스

---
+++ src/main/java/baseball/JudgeResult.java
@@ -0,0 +1,38 @@
+package baseball;
+
+public class JudgeResult {
+
+	private int strike;
+	private int ball;
+
+	public int getStrike() {
+		return strike;
+	}
+
+	public int getBall() {
+		return ball;
+	}
+
+	public void addBall(int ball) {
+		this.ball += ball;
+	}
+
+	public void addStrike(int strike) {
+		this.strike += strike;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		if (strike > 0) {
+			sb.append(strike + "스트라이크 ");
+		}
+		if (ball > 0) {
+			sb.append(ball + "볼");
+		}
+		if (strike == 0 && ball == 0) {
+			sb.append("낫싱");
+		}
+		return sb.toString();
+	}
+}
