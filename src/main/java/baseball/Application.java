7ffadfdd22c73e028ea1ff859f4a447ba8a5ea69
OHHYUN <omplcie@gmail.com>
2021-10-06 19:01:33 +0900

    refactor(baseball): 한 클래스 내에 있던 기능을 나눔

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,11 @@
 package baseball;
 
-import baseball.core.Baseball;
+import baseball.core.BaseballGame;
 
 public class Application {
 	public static void main(String[] args) {
 		// TODO 숫자 야구 게임 구현
-		Baseball baseball = new Baseball();
-		baseball.playGame();
+		BaseballGame baseballGame = new BaseballGame();
+		baseballGame.playGame();
 	}
 }
