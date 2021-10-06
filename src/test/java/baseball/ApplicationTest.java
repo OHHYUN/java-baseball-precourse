743ec1ea03612371be9a4eff1a719e4001fa67a9
OHHYUN <omplcie@gmail.com>
2021-10-06 18:32:11 +0900

    test(baseball): 게임 종료 후 재시작시 에러메세지 출력, 3자리 이상 숫자만 입력되게 됬는지 확인

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -38,6 +38,38 @@ public class ApplicationTest extends NSTest {
         }
     }
 
+    @Test
+    void 게임종료_후_재시작_에러출력() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                .thenReturn(713)
+                .thenReturn(589);
+            run("713", "3", "1", "589", "2");
+            verify("3스트라이크", "3개의 숫자를 모두 맞히셨습니다! 게임 끝",
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+                "[ERROR] 1이나 2를 입력해주세요.",
+                "3스트라이크",
+                "3개의 숫자를 모두 맞히셨습니다! 게임 끝",
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+    }
+
+    @Test
+    void 세글자_입력_검증() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                .thenReturn(589);
+            run("71544", "ㅁㄹㅁㄴㄹㄴ", "asdfsfaf", "$()*!@!#", "589", "2");
+            verify("[ERROR] : 3자리 숫자를 입력해주세요",
+                "[ERROR] : 3자리 숫자를 입력해주세요",
+                "[ERROR] : 3자리 숫자를 입력해주세요",
+                "[ERROR] : 3자리 숫자를 입력해주세요",
+                "3스트라이크",
+                "게임 끝"
+                );
+        }
+    }
+
     @AfterEach
     void tearDown() {
         outputStandard();
